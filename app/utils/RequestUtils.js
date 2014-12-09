a209f037f579aa53c4a341b49f57b03be7ea0184
Christian de Botton <cdebotton@gmail.com>
2014-12-08 23:19:16 -0500

    Cleaning up RequestAPIUtils.

--- app/utils/RequestUtils.js
+++ app/utils/RequestUtils.js
@@ -24,73 +24,51 @@ function digestPromise(resolve, reject) {
   };
 };
 
-exports.getQuery = function(url: string): request {
-  var getRequest;
-  return new Promise(function(resolve: Function, reject: Function): void {
-    getRequest = request.get(url)
-      .set('Authorization', 'Bearer ' + AuthStore.getToken())
-      .set('Accept', 'application/json')
-      .timeout(TIMEOUT)
-      .end(digestPromise(resolve, reject));
+function makePromise(action) {
+  return new Promise(function(resolve, reject) {
+    action.end(digestPromise(resolve, reject));
   })
-    .cancellable()
-    .catch(Promise.CancellationError, function(e) {
-      getRequest._callback = function() {};
-      getRequest.abort();
-      getRequest = null;
-    });
+  .cancellable()
+  .catch(Promise.CancellationError, function(e) {
+    action._callback = function() {};
+    action.abort();
+  });
+};
 
+exports.getQuery = function(url: string): request {
+  var action = request.get(url)
+    .set('Authorization', 'Bearer ' + AuthStore.getToken())
+    .set('Accept', 'application/json')
+    .timeout(TIMEOUT);
+
+  return makePromise(action);
 };
 
 exports.postQuery = function(url: string, params: Object): request {
-  var postRequest;
-  return new Promise(function(resolve: Function, reject: Function): void {
-    postRequest = request.post(url)
-      .set('Authorization', 'Bearer ' + AuthStore.getToken())
-      .set('Accept', 'application/json')
-      .send(params)
-      .timeout(TIMEOUT)
-      .end(digestPromise(resolve, reject));
-  })
-    .cancellable()
-    .catch(Promise.CancellationError, function(e) {
-      postRequest._callback = function() {};
-      postRequest.abort();
-      postRequest = null;
-    });
+  var action = request.post(url)
+    .set('Authorization', 'Bearer ' + AuthStore.getToken())
+    .set('Accept', 'application/json')
+    .send(params)
+    .timeout(TIMEOUT);
+
+  return makePromise(action);
 };
 
 exports.delQuery = function(url: string): request {
-  var delRequest;
-  return new Promise(function(resolve: Function, reject: Function): void {
-    delRequest = request.del(url)
+  var action = request.del(url)
     .set('Authorization', 'Bearer ' + AuthStore.getToken())
     .set('Accept', 'application/json')
-    .timeout(TIMEOUT)
-    .end(digestPromise(resolve, reject));
-  })
-    .cancellable()
-    .catch(Promise.CancellationError, function(e) {
-      delRequest._callback = function() {};
-      delRequest.abort();
-      delRequest = null;
-    });
+    .timeout(TIMEOUT);
+
+  return makePromise(action);
 };
 
 exports.putQuery = function(url: string, params: Object): request {
-  var putRequest;
-  return new Promise(function(resolve: Function, reject: Function): void {
-    putRequest = request.put(url)
-      .set('Authorization', 'Bearer ' + AuthStore.getToken())
-      .set('Accept', 'application/json')
-      .send(params)
-      .timeout(TIMEOUT)
-      .end(digestPromise(resolve, reject));
-  })
-    .cancellable()
-    .catch(Promise.CancellationError, function(e) {
-      putRequest._callback = function() {};
-      putRequest.abort();
-      putRequest = null;
-    });
+  var action = request.put(url)
+    .set('Authorization', 'Bearer ' + AuthStore.getToken())
+    .set('Accept', 'application/json')
+    .send(params)
+    .timeout(TIMEOUT);
+
+  return makePromise(action);
 };
